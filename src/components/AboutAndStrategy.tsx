8ea298ddd1e436944eb4816a55edd57704802d56
Teezi-App <support@teezi.golf>
2026-07-01 16:43:44 +0100

    commit files to git

--- src/components/AboutAndStrategy.tsx
+++ src/components/AboutAndStrategy.tsx
@@ -38,7 +38,7 @@ export default function AboutAndStrategy({ onScrollToSection }: AboutAndStrategy
                 With over 20 years of experience across the UK, Middle East and Africa, I've launched startups, led marketing in multi-million dollar businesses and helped small businesses to go digital.
               </p>
               <p className="font-medium text-neutral-900">
-                Marketni is how I help businesses grow with clarity, direction and purpose.
+                Marketni is how I help businesses to grow with clarity, direction and purpose.
               </p>
             </div>
           </div>
